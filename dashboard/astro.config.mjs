a2f14ba12d113df90f0bdb219c98080c912efde6
Stuart Mackenzie <ilovediscostu@gmail.com>
2026-03-29 16:57:39 +0100

    update site config

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  site: 'https://tgf-parkrun.hellostu.xyz',
   output: 'server',
   adapter: node({
     mode: 'standalone',
